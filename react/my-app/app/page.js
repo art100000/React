084c84cac9f01d52bc6f908a7f890407cd9c7451
art100000 <artfa9370@gmail.com>
2025-02-14 00:38:29 +0000

    aprendendendo react

---
+++ react/my-app/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+
+export default function Home() {
+const aula = 1;
+return(
+  <div>
+    <h1>Olá mundo!!!</h1>
+    <p>Essa é a nossa primeira {aula}ª aula de react</p>
+  </div>
+);
+
+}
